5deffd21422e02ab344926f5a9957a34e59429f2
Rajeev Kumar <rajeev.code@gmail.com>
2018-03-09 18:57:11 +0700

    Initial commit

---
+++ app/src/main/java/com/rajeevkumar/animation/MainActivity.java
@@ -0,0 +1,59 @@
+package com.rajeevkumar.animation;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+
+        ImageView shaun = (ImageView) findViewById(R.id.shaun);
+//
+//        shaun.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+
+        //shaun.animate().rotation(1800f).setDuration(2000);
+
+        //shaun.animate().translationXBy(1800f).setDuration(2000);
+
+        ImageView motu = (ImageView) findViewById(R.id.motu);
+
+        shaun.animate()
+                .translationXBy(1000f)
+                .translationYBy(1000f)
+                .rotationBy(3600)
+                .setDuration(3600);
+
+//        motu.animate().rotation(1800f).setDuration(2000);
+//        motu.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+
+       // motu.animate().scaleX(1f).scaleY(1f).setDuration(2000);
+
+        //motu.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+        //motu.animate().translationXBy(-1000f).setDuration(2000);
+
+        motu.animate().scaleXBy(0.2f).scaleY(0.2f).setDuration(2000);
+
+        //shaun.animate().alpha(0.5f).setDuration(2000);
+
+        //motu.animate().alpha(1f).setDuration(2000);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView shaun = (ImageView) findViewById(R.id.shaun);
+//
+//        shaun.animate().translationXBy(-1000f);
+//
+        ImageView shaun = (ImageView) findViewById(R.id.shaun);
+        shaun.setTranslationX(-1000f);
+        shaun.setTranslationY(-1000f);
+//        motu.animate().translationXBy(1000f).setDuration(2000);
+    }
+}
