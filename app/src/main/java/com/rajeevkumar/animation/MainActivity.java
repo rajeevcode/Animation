85dd5ff1f9763438483b4ae7015d74c1556e9c9c
Rajeev Kumar <rajeev.code@gmail.com>
2018-03-10 00:10:50 +0700

    added code import android.support.v4.view to support legacy OS version

--- app/src/main/java/com/rajeevkumar/animation/MainActivity.java
+++ app/src/main/java/com/rajeevkumar/animation/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.support.v4.view.ViewCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
 
         ImageView shaun = (ImageView) findViewById(R.id.shaun);
+        ViewCompat.animate(shaun);
 //
 //        shaun.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
 
